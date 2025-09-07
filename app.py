b6d03bb0c6db60c59fc19c2718071e67ef262d8e
Jayant Maheshwari <76027091+jayant013@users.noreply.github.com>
2025-09-07 17:50:10 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+#login and register
+#gforms integration
+#while loop check index
+#close regis when 100reg (idt this is necessary ~ j)
+#speaker eegister section
+#event page uses db tables and inputoutput
