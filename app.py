7c23b3bad75e25d1e411cc1fa9dadc1fa1515078
Yao <156203035+YakAliOen@users.noreply.github.com>
2025-09-13 11:37:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ Session(app)
 # Home page
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index1.html")
 
 # Newsletter subscription
 @app.route("/subscribe", methods=["POST"])
@@ -43,3 +43,10 @@ def about():
   
 #tickets tab
   #Display Hash Key/QR Code according to db
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
